d5a0b7dc49ac01143f8276a0dc30232889faf22b
Thabang Git <tmzamane85@yahoo.com>
2022-06-08 23:27:55 +0200

    DB error fixing complete

--- db/migrate/20220608205831_add_timestamps_to_migration.rb
+++ db/migrate/20220608205831_add_timestamps_to_migration.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToMigration < ActiveRecord::Migration[5.2]
   def change
-      add_column :articles :created_at, :datetime
-      add_column :artilces :updated_at, :datetime
+      add_column :articles, :created_at, :datetime
+      add_column :articles, :updated_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_233242) do
+ActiveRecord::Schema.define(version: 2022_06_08_205831) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
