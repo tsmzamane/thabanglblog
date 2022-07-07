7e3f06bd142418e674b7c13153354a9d8156bcc7
Thabang Git <tmzamane85@yahoo.com>
2022-07-07 23:00:22 +0200

    Add one-to-many association between users and articles from backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_184406) do
+ActiveRecord::Schema.define(version: 2022_07_05_193102) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
